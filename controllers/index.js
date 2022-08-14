614003d02cd9977ab2e47740c390168f02ebd59d
PhungThinhVuong <phungthinhvuong2101@gmail.com>
2022-08-14 23:27:15 +0700

    fix

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,6 @@
-/** Xử lý chức năng giao diện */
 
 var staffList = [];
-
 function createStaff() {
-  // 1. lấy thông tin từ input
   var staffID = document.querySelector("#tknv").value;
   var staffName = document.querySelector("#name").value;
   var staffEmail = document.querySelector("#email").value;
@@ -13,7 +10,6 @@ function createStaff() {
   var staffLevel = document.querySelector("#chucvu").value;
   var staffWorkTime = document.querySelector("#gioLam").value;
 
-  // 2. tạo đối tượng nhan vien
 
   var staff = new NhanVien(
     staffID,
@@ -28,13 +24,10 @@ function createStaff() {
 
   console.log(staff);
 
-  ///* ------------------------- Kiểm tra dữ liệu đầu vào ----------------------------------
-  /*
-     Kiểm tra rỗng
-   */
+ 
 
   var valid = true;
-  //.trim(): Loại bỏ khoảng trắng đầu và cuối của chuỗi Ví dụ:     => ''
+  
   if (staff.id.trim() === "") {
     document.querySelector("#tbTKNV").innerHTML =
       "Mã nhân viên không được bỏ trống";
@@ -49,14 +42,12 @@ function createStaff() {
   if (!valid) {
     return;
   }
-
-  // 3. push đối tượng nhân viên vào danh sách
+  
   staffList.push(staff);
-  //Gọi hàm tạo table sau khi thêm 1 nhân viên mới vào
-  // console.table(studentList);
+  
   renderStaffList(staffList);
 
-  //gọi hàm lưu vào localstorage sau khi thêm sinh viên
+  
   saveLocalStorage(staffList, "arrNV");
 }
 
@@ -123,13 +114,13 @@ function editStaff(idClick) {
   var nvEdit = null;
   for (var index = 0; index < staffList.length; index++) {
     if (staffList[index].id == idClick) {
-      //Tại vị trí này tìm thấy idClick = id object trong mảng
+      
       nvEdit = staffList[index];
       break;
     }
   }
   if (nvEdit !== null) {
-    //Đưa dữ liệu lên các control input
+    
     document.querySelector("#tknv").value = nvEdit.id;
     document.querySelector("#name").value = nvEdit.name;
     document.querySelector("#email").value = nvEdit.email;
@@ -144,21 +135,21 @@ function editStaff(idClick) {
 function deleteStaff(idClick) {
   var indexDel = -1;
   for (var index = 0; index < staffList.length; index++) {
-    // mỗi lần duyệt lấy ra 1 phần tử mảng so với input người dùng click
+    
     if (staffList[index].id === idClick) {
       indexDel = index;
-      break; // thoát ra khỏi vòng lặp
+      break; 
     }
   }
   if (indexDel !== -1) {
-    //tìm thấy
+    
     staffList.splice(indexDel, 1);
     renderStaffList(staffList);
     saveLocalStorage(staffList, "arrNV");
   }
 }
 
-//Khi người dùng thay đổi sau đó bấm nút update
+
 function updateStaff() {
   var nvUpdate = new NhanVien();
   nvUpdate.id = document.querySelector("#tknv").value;
@@ -184,7 +175,7 @@ function updateStaff() {
     staffList[indexEdit].day = nvUpdate.day;
     staffList[indexEdit].level = nvUpdate.level;
     staffList[indexEdit].workTime = nvUpdate.workTime;
-    //Gọi hàm rendertable truyền cho hàm mảng mới
+    
     renderStaffList(staffList);
 
 
@@ -192,23 +183,23 @@ function updateStaff() {
 }
 
 function saveLocalStorage(ob, key) {
-  // {} , []
+ 
   var str = JSON.stringify(ob);
   localStorage.setItem(key, str);
 }
 
 function getLocalStorage(key) {
-  //Lấy dữ liệu từ localstorage ra (dữ liệu lấy là string)
+  
   if (localStorage.getItem(key)) {
     var str = localStorage.getItem(key);
-    //Parse dữ liệu về lại object
+    
     var ob = JSON.parse(str);
     return ob;
   }
   return undefined;
 }
 
-//đợi html js load xong thì sẽ động thực thi
+
 window.onload = function () {
   studentList = getLocalStorage("arrNV");
   console.log("staffList", staffList);
@@ -228,12 +219,12 @@ var searchStaff = function () {
       output.push(staffList[index]);
     }
   }
-  //Dùng output render ra table
+  
   renderStaffList(output);
 };
-//Dom đến txtSearch cài đặt sự kiện oninput cho nó
+
 document.querySelector("#searchName").oninput = searchStaff;
-//Tìm kiếm
+
 document.querySelector("#btnTimNV").onclick = searchStaff;
 
 function removeVietnameseTones(str) {
@@ -251,16 +242,13 @@ function removeVietnameseTones(str) {
   str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
   str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
   str = str.replace(/Đ/g, "D");
-  // Some system encode vietnamese combining accent as individual utf-8 characters
-  // Một vài bộ encode coi các dấu mũ, dấu chữ như một kí tự riêng biệt nên thêm hai dòng này
-  str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
-  str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
-  // Remove extra spaces
-  // Bỏ các khoảng trắng liền nhau
+  
+  str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); 
+  str = str.replace(/\u02C6|\u0306|\u031B/g, ""); 
+  
   str = str.replace(/ + /g, " ");
   str = str.trim();
-  // Remove punctuations
-  // Bỏ dấu câu, kí tự đặc biệt
+  
   str = str.replace(
     /!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g,
     " "
@@ -268,80 +256,3 @@ function removeVietnameseTones(str) {
   return str;
 }
 
-// // Get data
-
-// function getDataStaffApi() {
-//   var promise = axios({
-//     url: "../data/data.json",
-//     method: "GET",
-//   });
-
-//   //thanh cong
-//   promise.then(function (result) {
-//     console.log("result", result.data);
-//     renderNhanVien(result.data);
-//   });
-
-//   //that bai
-//   promise.catch(function (err) {
-//     console.log(err);
-//   });
-// }
-
-// window.onload = function () {
-//   //goi api
-//   getDataStaffApi();
-// };
-
-// function renderNhanVien(arrNhanVien) {
-//   var html = "";
-//   for (var i = 0; i < arrNhanVien.length; i++) {
-//     var nv = arrNhanVien[i];
-//     html += `
-//         <tr>
-//         <td>${nv.maNhanVien}</td>
-//         <td>${nv.tenNhanVien}</td>
-//         <td>${nv.email}</td>
-//         <td>${nv.ngaylam}</td>
-//         <td>${nv.chucVu}</td>
-//         <td></td>
-//         <td></td>
-//         <td>
-//             <button class="btn btn-primary" onclick="chinhsua('${nv.maNhanVien}')">Sửa</button>
-//             <button class="btn btn-danger" onclick="xoaNhanVien('${nv.maNhanVien}')">Xóa</button>
-//         </td>
-//         </tr>
-//         `;
-//   }
-//   document.querySelector('tbody').innerHTML = html;
-// }
-
-// /** POST: thêm dữ liệu */
-// document.querySelector('#btnThemNV').onclick = function(){
-//     // laasy thoong tin tu nguoi dung
-//     var nhanVien = new NhanVien();
-//     nhanVien.maNhanVien = document.querySelector('#tknv').value;
-//     nhanVien.tenNhanVien = document.querySelector('#name').value;
-//     nhanVien.email = document.querySelector('#email').value;
-//     nhanVien.password = document.querySelector('#password').value;
-//     nhanVien.ngaylam = document.querySelector('#datepicker').value;
-//     nhanVien.luongCoBan = document.querySelector('#luongCB').value;
-//     nhanVien.chucVu = document.querySelector('#chucvu').value;
-//     nhanVien.soGioLamTrongThang = document.querySelector('#gioLam').value;
-//     console.log('nhanVien',nhanVien);
-
-//     var promise = axios({
-//         url:'../data/data.json',
-//         method:'POST',
-//         // data:nhanVien
-//     })
-
-//     promise.then(function(result){
-//         console.log(result.data);
-//         getDataStaffApi();
-//     })
-
-//     promise.catch(function(error){
-//         console.log(error);
-//     })
-// }
